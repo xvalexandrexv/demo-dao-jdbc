16b4ca25f0bdfa60d83634dfbe0c42d181086e56
Alexandre Silva <xvalexandrexv@gmail.com>
2024-02-25 00:49:35 +0000

    added seller

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,16 @@
 package app;
 
 import Model.Department;
+import Model.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
 
 
     }
