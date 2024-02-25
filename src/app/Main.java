21d5f8b31dd3bc666168cb9e90087868d44d44a1
Alexandre Silva <xvalexandrexv@gmail.com>
2024-02-25 00:41:12 +0000

    Department entities class

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import Model.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
+
+    }
+}
